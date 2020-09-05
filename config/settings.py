de783cf5117184ec584a1614b55548238fffe90a
y-u-y-a <yuya.ymd00@gmail.com>
2020-09-05 14:39:04 +0900

    マイグレーション完了
    DBの設定を定義
    モデル作成
    makemigrationsを実行
    migrateを実行

--- config/settings.py
+++ config/settings.py
@@ -61,13 +61,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD']
     }
 }
 
--- device/models.py
+++ device/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+def load_path(instance, filename):
+    path = '/'.join(['image', str(instance.title) + str('.jpg')])
+    return path
+
+
+class Device(models.Model):
+
+    class Meta:
+        db_table = 'device' # device_deviceになってしまうため
+
+    # blank: 入力必須, null: 空許可
+    title = models.CharField(max_length=30, blank=False)
+    image = models.ImageField(blank=True, null=True, upload_to=load_path)
+
+    # インスタンスにstrやprint関数を使うと__str__メソッドの戻り値が取得できる
+    # このクラスのインスタンスの戻り値を指定
+    def __str__(self):
+        return self.title
+
