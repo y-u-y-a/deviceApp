f3e5749afbea63a7778cec4f8d47c8ccf47ef0c4
y-u-y-a <yuya.ymd00@gmail.com>
2020-09-06 16:46:54 +0900

    修正

--- core/serializers/userSerializer.py
+++ core/serializers/userSerializer.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
+
 from core.models import User
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
 
+    class Meta:
         # 公開する値
         model = User
         fields = ['id', 'name', 'email']
