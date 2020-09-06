b5786015107805848ac140fa61f819afde8008ac
y-u-y-a <yuya.ymd00@gmail.com>
2020-09-06 10:06:31 +0900

    ルーティングをconfig/urls.pyに集約

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include # 追加
+from django.urls import path, include
+from rest_framework import routers
 
 from django.conf.urls.static import static
 from django.conf import settings
+# viewsets
+from device.views import DeviceViewSet
+
+
+# create router instance
+router = routers.DefaultRouter()
+router.register('devices', DeviceViewSet) # 'api/device/'
+
 
 urlpatterns = [
-    # 管理画面
+    # admin
     path('admin/', admin.site.urls),
-    # 追加
-    path('api/device/', include('device.urls')),
+    # api
+    path('api/', include(router.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
